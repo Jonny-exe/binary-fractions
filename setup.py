6b4acdf6fd89c51e0f0f36f69010d0c577d90b72
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-06-19 19:11:10 +0200

    update

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ classifiers = []
 
 setup(
     name="binary_fractions",
-    version="0.0.2",
-    description="A package to handle binary fractions",
+    version="0.1.0",
+    description="A Python package for floating-point binary fractions. Do math in base 2!",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Jonny-exe/binary-fractions",
@@ -13,7 +13,7 @@ setup(
     author_email="",
     license="MIT",
     classifiers=classifiers,
-    keywords="binary fractions binary-fractions",
+    keywords="binary fractions python python3 floating-point high-precision base-2 floating-point-arithmetic binary-fractions",
     install_requires=[""],
     packages=["binary_fractions"]
 )
