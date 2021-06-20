ee52ae36ea7cfe61f4cbe42e26345aaa76a5770c
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-06-20 16:48:36 +0200

    updated setuphook script

--- setuphook.sh
+++ setuphook.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 #
-cnt=$(grep "# autogenerate README.md" .git/hooks/pre-commit.sample | wc -l)
+cnt=$(grep "# autogenerate README.md" .git/hooks/pre-commit | wc -l)
 if [ "$cnt" == "2" ]; then
   return
 fi
-echo "" >> .git/hooks/pre-commit.sample 
-echo "# autogenerate README.md" >> .git/hooks/pre-commit.sample 
-echo "pydoc-markdown -I ../../binary_fractions/ -m binary --render-toc > ../../binary_fractions/ README.md" >> .git/hooks/pre-commit.sample 
-echo "echo 'Generated new Documents'" >> .git/hooks/pre-commit.sample 
+echo "" >> .git/hooks/pre-commit
+echo "# autogenerate README.md" >> .git/hooks/pre-commit
+echo "pydoc-markdown -I ../../binary_fractions/ -m binary --render-toc > ../../binary_fractions/ README.md" >> .git/hooks/pre-commit
+echo "echo 'Generated new Document README.md'" >> .git/hooks/pre-commit
