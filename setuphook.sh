2e46e653287cd49ee8f9a17d7b9381d48f513abb
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-06-20 17:53:26 +0200

    fixed bugs

--- setuphook.sh
+++ setuphook.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
-#
-cnt=$(grep "# autogenerate README.md" .git/hooks/pre-commit | wc -l)
-if [ "$cnt" == "2" ]; then
-  return
+HOOKFILE=".git/hooks/pre-commit"
+PATTERN="# autogenerate README.md"
+cnt=$(grep "$PATTERN" $HOOKFILE | wc -l)
+if [ "$cnt" == "0" ]; then
+  #
+  sed -i "s/# Cross platform projects tend to avoid non-ASCII filenames; prevent/\n\n$PATTERN\npydoc-markdown -I .\/binary_fractions -m binary --render-toc  > .\/binary_fractions\/README.md\necho Generated new document README.md\n\n\n# Cross platform projects tend to avoid non-ASCII filenames; prevent/" $HOOKFILE
+  echo "Modified file $HOOKFILE"
+else
+  echo "Nothing to do, setup was already done in $HOOKFILE"
 fi
-echo "" >> .git/hooks/pre-commit
-echo "# autogenerate README.md" >> .git/hooks/pre-commit
-echo "pydoc-markdown -I ../../binary_fractions/ -m binary --render-toc > ../../binary_fractions/ README.md" >> .git/hooks/pre-commit
-echo "echo 'Generated new Document README.md'" >> .git/hooks/pre-commit
+# EOF
