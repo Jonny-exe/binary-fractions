245848372d36f0f3c4c020c5c67b98ad36d62081
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-06-20 16:43:08 +0200

    add hook setupt

---
+++ setuphook.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+#
+cnt=$(grep "# autogenerate README.md" .git/hooks/pre-commit.sample | wc -l)
+if [ "$cnt" == "2" ]; then
+  return
+fi
+echo "" >> .git/hooks/pre-commit.sample 
+echo "# autogenerate README.md" >> .git/hooks/pre-commit.sample 
+echo "pydoc-markdown -I ../../binary_fractions/ -m binary --render-toc > ../../binary_fractions/ README.md" >> .git/hooks/pre-commit.sample 
